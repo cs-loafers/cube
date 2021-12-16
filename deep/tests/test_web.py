71ecbeefdab42cb36c145524ebd6f4f24012754d
OLI370 <767674751@qq.com>
2021-12-15 21:41:29 -0800

    改变travis.yml配置

--- deep/tests/test_web.py
+++ deep/tests/test_web.py
@@ -6,16 +6,15 @@ driver.get('http://www.baidu.com')
 print driver.title  
 driver.quit()  
 
-#driver = webdriver.Chrome()
-#driver.get("http://localhost:8000/index/")
+driver = webdriver.Chrome()
+driver.get("http://localhost:8000/index/")
 
-#ele = driver.find_element_by_xpath('/html/body/form/input[7]')
-#ele.click()
-#sleep(2)
-#if ele.is_selected():
-#    print('pass')
-#sleep(3)
+driver.find_element_by_id("scramble").click()
 
+driver.find_element_by_id("solve").click()
+
+sleep(3)
 
 
-#driver.quit()
+
+driver.quit()
